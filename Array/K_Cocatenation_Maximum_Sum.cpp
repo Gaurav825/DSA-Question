70ceb465989a30b10a31fba1b1c1201b7b3785ee
GAURAV <64942636+Gaurav825@users.noreply.github.com>
2023-07-30 11:12:38 +0530

    Update K_Cocatenation_Maximum_Sum.cpp

--- Array/K_Cocatenation_Maximum_Sum.cpp
+++ Array/K_Cocatenation_Maximum_Sum.cpp
@@ -1,3 +1,27 @@
+//if k=1 return kadane(a)
+/*
+if(sum<0)
+return kadane(a+a);
+if(sum>0)
+{
+return sum*(k-2)+kadane(a+a)
+// if(sum>0) 
+=> kadane(a)<kadane(a+a);
+because sum(a)>0 so used whole array sum which is positive + max sum subaaray of one array
+ex-> -3 3 4 -2
+       maxsubsum(3,4)=7
+        but -3 3 4 -2 -3 3 4 -2>
+        maxsubarray (3+4+-2-3+3+4)=>9 hence proved         
+
+
+}
+
+
+
+ */
+
+
+
 int kConcatenationMaxSum(vector<int>& a, int k) {
          int m_sum = 0, sz = a.size();
     for (auto i = 0, sum = 0; i < min(2, k) * sz; ++i) {
